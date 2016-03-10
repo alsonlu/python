0ec255c26d3fc59668ee592426e58f3fd6c2da64
unknown <alsonlu@manahealth.com>
2016-03-10 18:48:12 -0500

    Added own function and turtle

---
+++ InterfaceDesign/Turtle/mypolygon.py
@@ -0,0 +1,11 @@
+import turtle
+
+bob = turtle.Turtle()
+
+for i in range(4):
+    bob.fd(100)
+    bob.lt(90)
+
+# .mainloop tell the window to wait for the user to do something
+# turtle.mainloop()
+
