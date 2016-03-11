591953117cf21ff37d2bb3bec9a80b4756843d6f
Alson Lu <alson.lu@gmail.com>
2016-03-11 00:59:50 -0500

    Added more to turtle

--- InterfaceDesign/Turtle/mypolygon.py
+++ InterfaceDesign/Turtle/mypolygon.py
@@ -2,10 +2,32 @@ import turtle
 
 bob = turtle.Turtle()
 
-for i in range(4):
-    bob.fd(100)
-    bob.lt(90)
-
 # .mainloop tell the window to wait for the user to do something
 # turtle.mainloop()
+
+
+def square(t, length):
+    for i in range(4):
+        t.fd(length)
+        t.lt(90)
+
+# square(bob, 300)
+
+
+def polygon(t, length, n):
+    for i in range(n):
+        t.fd(length)
+        t.lt(360 / n)
+
+# polygon(bob, 100, 8)
+
+""" This is a multiline....
+comment
+"""
+
+
+def circle(t, r):
+    for i in range(100):
+        t.fd(5)
+        t.lt(360 / 100)
 
